79e722c37f65e21799e5bbdc6c4c01b577ac4546
Lendi <lendivihari@gmail.com>
2018-05-30 13:43:30 +0530

    added server.py

--- server.py
+++ server.py
@@ -1,6 +1,12 @@
 from flask import Flask
 from flask_restplus import Resource, Api, fields
 from flask_cors import CORS, cross_origin
+from language_processor import train_processor, interpret_data_for
+import os
+
+print "Starting language processor..."
+train_processor(os.path.join(os.path.dirname(__file__), 'nlu/training_data.json'), os.path.join(os.path.dirname(__file__), 'nlu/config.yml'))
+print "Ended training dataset. Ready to take the hit..."
 
 app = Flask(__name__)
 api = Api(app)
@@ -10,6 +16,12 @@ CORS(app)
 class Home(Resource):
     def get(self):
         return {'hello': 'world'}
+
+@api.route('/nlpQuery/<string:query>')
+class QueryResource(Resource):
+	def get(self, query):
+		result = interpret_data_for(query)
+		return {'result':result}
 
 
 if __name__ == "__main__":
